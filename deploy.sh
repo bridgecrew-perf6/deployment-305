bdf90e66908da7494a7fdde666c22a7290573373
sumukshashidhar <sumukuuu@gmail.com>
2022-01-03 22:02:26 -0800

    feat[deploy]: add redis

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 echo "DEPLOYMENT IN PROGRESS"
 # there are two versions of the possible deployment.
 # we will create two files: one for a lean deployment, one for a full deployment
-
+git submodule init
+git submodule update
 
 
 # DATA FOLDER CREATION
@@ -11,6 +12,7 @@ mkdir ./data
 ## DATABASE SECTION
 mkdir ./data/mongo
 mkdir ./data/neo4j
+mkdir ./data/redis
 
 
 # ENV FILE CREATION
@@ -20,5 +22,10 @@ mkdir ./data/neo4j
 cp ./databases/mongo/.env.template ./databases/mongo/.env
 ### neo4j
 cp ./databases/neo4j/.env.template ./databases/neo4j/.env
+### redis
+cp ./databases/redis/.env.template ./databases/redis/.env
 ## DATABASE INTERACTORS
 cp ./microservices/external/mongo-express/.env.template ./microservices/external/mongo-express/.env
+
+# run docker
+docker-compose up
